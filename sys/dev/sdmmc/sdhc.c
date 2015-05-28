f2e7cc1185756090d9797fb3eec391aaddc4b622
Patrick Wildt <patrick@blueri.se>
2015-05-28 13:25:54 +0200

    sdhc: wait for busy signal to be released
    If the command protocol calls for a trailing busy signal,
    wait (up to 10 seconds) for it to be released.
    
    From NetBSD.

--- sys/dev/sdmmc/sdhc.c
+++ sys/dev/sdmmc/sdhc.c
@@ -858,6 +858,13 @@ sdhc_exec_command(sdmmc_chipset_handle_t sch, struct sdmmc_command *cmd)
 	 */
 	if (cmd->c_error == 0 && cmd->c_data != NULL)
 		sdhc_transfer_data(hp, cmd);
+	else if (ISSET(cmd->c_flags, SCF_RSP_BSY)) {
+		if (!sdhc_wait_intr(hp, SDHC_TRANSFER_COMPLETE, hz * 10)) {
+			cmd->c_error = ETIMEDOUT;
+			SET(cmd->c_flags, SCF_ITSDONE);
+			return;
+		}
+	}
 
 	/* Turn off the LED. */
 	HCLR1(hp, SDHC_HOST_CTL, SDHC_LED_ON);
